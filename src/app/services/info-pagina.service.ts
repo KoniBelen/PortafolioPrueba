8e933674e1c5ef31516f8e9a47819b160c7380fb
KoniBelen <konibelen12@gmail.com>
2020-08-15 15:18:35 -0400

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,22 +10,35 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient) { 
 
-    //console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
+
+     //Leer el archivo JSON
+     this.http.get('assets/data/data-pagina.json')
+     .subscribe((resp: InfoPagina) => {
+ 
+       this.cargada = true;
+       this.info = resp;
+       
+       //console.log(resp);
+       // console.log( resp ['email']);
+     });
+
+  }
   
-    //Leer el archivo JSON
-    
-    this.http.get('assets/data/data-pagina.json')
-    .subscribe((resp: InfoPagina) => {
-
-      this.cargada = true;
-      this.info = resp;
+  private cargarEquipo(){
+    this.http.get('https://angular-html-5ddaf.firebaseio.com/equipo.json')
+    .subscribe((resp: any[]) => {
       
-      console.log(resp);
-      // console.log( resp ['email']);
+      this.equipo = resp;
+      //console.log(resp);
     });
-
-
   }
 }
